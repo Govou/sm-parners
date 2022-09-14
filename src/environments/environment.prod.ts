276d6657862bda2fc4905260ac4017fdbc169b14
Kehinde Guvoeke <Govou11@outlook.com>
2022-09-14 18:38:52 +0100

    changed env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  halobizBaseUrl: 'https://dev-online-backend.azurewebsites.net',
+  defaultImageUrl: '../assets/images/CameraShutterColor.svg',
+  paystackKey: 'pk_test_61980d19f89fe51c0bca4d5dbcf0e669be155447',
+  flutterwaveKey: 'FLWPUBK_TEST-540620e287966341633afd1b6a789d9b-X',
+  halobizUsername: 'online.portal@halogen-group.com',
+  halobizPassword: 'rD:yfBj8;?%cdp*&38k',
+   firebaseConfig: {
+    apiKey: "AIzaSyCdQkShCc1pTOquUcBLKkPN1RX3JtdOtXI",
+    authDomain: "my-halobiz-projects.firebaseapp.com",
+    projectId: "my-halobiz-projects",
+    storageBucket: "my-halobiz-projects.appspot.com",
+    messagingSenderId: "937706939066",
+    appId: "1:937706939066:web:eeb95ddb7b23cc71fd1883",
+    measurementId: "G-XV5PPFGJ4P"
+  }
 };
